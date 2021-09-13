41a274b9e02f9fe6418ecfe482c740a91b5fe61f
Ness <nayos144@gmail.com>
2021-09-13 03:26:28 -0600

    update user controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
     #validates :rol_id, presence: true, on: :create
 
     #attr_accessor :rol_id, :name, :lastname, :email, :phone, :created_at, :updated_at
-    attr_accessor :password_digest
+    #attr_accessor :password_digest
 end
